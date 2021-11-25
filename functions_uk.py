54dc03c5bf048aa187e76b281537240b2d98eff9
Bhrenner Wilson <bhrenner_wilson@hotmail.com>
2021-11-25 18:54:45 -0300

    Add Doc

--- functions_uk.py
+++ functions_uk.py
@@ -1,13 +1,15 @@
 import datetime
 
 def process_time():
+    """Retorna uma variavel da hora em que a função é chamada"""
     data = datetime.datetime.now()
     data = data.strftime('%d/%m/%Y %H:%M')
     return data
 
 def acha_regiao(dicio):
+    """Cria uma lista com as regiões que não possuem vais de 2 crimes"""
     elimina_regiao = []
     for reg in dicio:
-        if len(dicio[reg]) < 2:
+        if len(dicio[reg])<2:
             elimina_regiao.append(reg)
     return elimina_regiao
--- uk_crimes.py
+++ uk_crimes.py
@@ -1,13 +1,16 @@
 import csv
 import functions_uk
-dicio = {}
+dicio:dict = {} 
 data = functions_uk.process_time()
 
+""" Abre um arquivo csv """
 with open('crimes_uk/reccrimepfa-210901-151708.csv', 'r') as arquivo: 
-    arquivo = csv.reader(arquivo, delimiter=",") 
+    reader = csv.reader(arquivo, delimiter=",") 
     header = next(arquivo)
-    #row = [data, pfa, regiao, crime, quantidade]
-    for row in arquivo:
+    """Laço de repetiçao que insere e filtra cada linha 
+    caso não esteja no dicionário evitando repetiões
+    e agrupando valores iguais"""
+    for row in reader:
         if row[2] not in dicio:
            dicio[row[2]] = {}
         if int(row[4]) > 10:
@@ -15,17 +18,18 @@ with open('crimes_uk/reccrimepfa-210901-151708.csv', 'r') as arquivo:
                 dicio[row[2]][row[3]] = []
             dicio[row[2]][row[3]].append(int(row[4]))
         for key in dicio[row[2]]:   
-            if len(dicio[row[2]][key]) >= 2:    
+            if len(dicio[row[2]][key])>=2:    
                 soma = sum(dicio[row[2]][key])
                 dicio[row[2]][key].clear()
                 dicio[row[2]][key].append(soma)
 
-elimina_regiao = functions_uk.acha_regiao(dicio)               
-for x in elimina_regiao:
+elimina_regiao = functions_uk.acha_regiao(dicio)  #Retorna uma lista de regioes que possuem poucos crimes            
+for x in elimina_regiao: #Elimina do dicionáio cada região com menos de 2 crimes
     if x in dicio:
         dicio.pop(x)
         
 with open('crimes_uk/resultado.JSON', 'w+') as arq:
+    """Cria um arquivo JSON"""
     for reg in dicio:
         arq.write('\n process time: ' + str(data) + ' region: ' + str(reg) + ' crimes: ' + str(dicio[reg]))
 
